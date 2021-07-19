3cb957a1cffd302efbd908c4f14db739571f50a0
Sinclert Pérez <Sinclert@hotmail.com>
2021-07-19 11:57:01 +0200

    Define input content file source

---
+++ src/input/content/corpus.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from dialect_map_io import PDFTextParser
+
+
+class ArxivCorpusOperator:
+    """File corpus operator for the ArXiv PDFs"""
+
+    def __init__(self, parser: PDFTextParser):
+        """
+        Initializes the corpus operator with a given parser
+        :param parser: object to parse the PDF files
+        """
+
+        self.parser = parser
+
+    def extract_txt(self, file_path: str) -> str:
+        """
+        Extracts the raw text out of a PDF file
+        :param file_path: path to the PDF file
+        :return: file text
+        """
+
+        return self.parser.parse_file(file_path)
