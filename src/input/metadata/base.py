b7dbf8285f8cccf3a60011438d399367ad496d11
Sinclert Pérez <Sinclert@hotmail.com>
2021-10-18 15:44:16 -0400

    Adapt existing input classes

--- src/input/metadata/base.py
+++ src/input/metadata/base.py
@@ -4,14 +4,14 @@ from abc import ABC
 from abc import abstractmethod
 from typing import List
 
-from dialect_map_io import ArxivFeedEntry
+from ...models import ArxivMetadata
 
 
 class BaseMetadataSource(ABC):
     """Interface for the metadata sources"""
 
     @abstractmethod
-    def get_metadata(self, paper_id: str) -> List[ArxivFeedEntry]:
+    def get_metadata(self, paper_id: str) -> List[ArxivMetadata]:
         """
         Retrieves the complete metadata of the multiple ArXiv paper versions
         :param paper_id: ArXiv paper ID
