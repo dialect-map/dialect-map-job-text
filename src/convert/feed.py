d412b493b6507caaa7f4e4582f190f1905d3b8d7
Sinclert Pérez <Sinclert@hotmail.com>
2021-10-18 15:44:03 -0400

    Adapt existing convert classes

--- src/convert/feed.py
+++ src/convert/feed.py
@@ -2,74 +2,75 @@
 
 from typing import List
 
-from dialect_map_io import ArxivFeedEntry
 from dialect_map_schemas import CategoryMembershipSchema
 from dialect_map_schemas import PaperSchema
 from dialect_map_schemas import PaperAuthorSchema
+
+from ..models import ArxivMetadata
 
 
 class ArxivFeedConverter:
     """ArXiv feed to Dialect map records converter"""
 
     @staticmethod
-    def get_paper_data(feed_entry: ArxivFeedEntry) -> dict:
+    def get_paper_data(metadata: ArxivMetadata) -> dict:
         """
-        Adapts one of the ArXiv feed entry information into a Paper record
-        :param feed_entry: ArXiv feed entry information
+        Adapts one of the ArXiv metadata entries into a Paper record
+        :param metadata: ArXiv metadata entry
         :return: Paper record
         """
 
         schema = PaperSchema()
         record = schema.load(
             {
-                "arxiv_id": feed_entry.paper_id,
-                "arxiv_rev": feed_entry.paper_rev,
-                "title": feed_entry.paper_title,
-                "doi_id": feed_entry.paper_doi,
-                "revision_date": feed_entry.paper_updated_at.date(),
-                "submission_date": feed_entry.paper_created_at.date(),
-                "created_at": feed_entry.paper_created_at,
-                "updated_at": feed_entry.paper_updated_at,
+                "arxiv_id": metadata.paper_id,
+                "arxiv_rev": metadata.paper_rev,
+                "title": metadata.paper_title,
+                "doi_id": metadata.paper_doi,
+                "revision_date": metadata.paper_updated_at.date(),
+                "submission_date": metadata.paper_created_at.date(),
+                "created_at": metadata.paper_created_at,
+                "updated_at": metadata.paper_updated_at,
             }
         )
 
         return schema.dump(record)
 
     @staticmethod
-    def get_paper_authors(feed_entry: ArxivFeedEntry) -> List[dict]:
+    def get_paper_authors(metadata: ArxivMetadata) -> List[dict]:
         """
-        Adapts one of the ArXiv feed entry information into several PaperAuthor records
-        :param feed_entry: ArXiv feed entry information
+        Adapts one of the ArXiv metadata entries into several PaperAuthor records
+        :param metadata: ArXiv metadata entry
         :return: PaperAuthor records
         """
 
         shared = {
-            "arxiv_id": feed_entry.paper_id,
-            "arxiv_rev": feed_entry.paper_rev,
-            "created_at": feed_entry.paper_created_at,
+            "arxiv_id": metadata.paper_id,
+            "arxiv_rev": metadata.paper_rev,
+            "created_at": metadata.paper_created_at,
         }
 
         schemas = PaperAuthorSchema(many=True)
-        records = [{"author_name": author.name, **shared} for author in feed_entry.paper_authors]
+        records = [{"author_name": author.name, **shared} for author in metadata.paper_authors]
         records = schemas.load(records)
 
         return schemas.dump(records)
 
     @staticmethod
-    def get_paper_membership(feed_entry: ArxivFeedEntry) -> dict:
+    def get_paper_membership(metadata: ArxivMetadata) -> dict:
         """
-        Adapts one of the ArXiv feed entry information into a CategoryMembership record
-        :param feed_entry: ArXiv feed entry information
+        Adapts one of the ArXiv metadata entries into a CategoryMembership record
+        :param metadata: ArXiv metadata entry
         :return: CategoryMembership record
         """
 
         schema = CategoryMembershipSchema()
         record = schema.load(
             {
-                "arxiv_id": feed_entry.paper_id,
-                "arxiv_rev": feed_entry.paper_rev,
-                "category_id": feed_entry.paper_category,
-                "created_at": feed_entry.paper_created_at,
+                "arxiv_id": metadata.paper_id,
+                "arxiv_rev": metadata.paper_rev,
+                "category_id": metadata.paper_category,
+                "created_at": metadata.paper_created_at,
             }
         )
 
