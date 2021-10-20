e88fd3484ee570cf0ac47c37b9ebe1a9403eb411
Sinclert Pérez <Sinclert@hotmail.com>
2021-10-20 11:53:38 -0400

    Move metadata routes into own folder

--- src/mapping/__init__.py
+++ src/mapping/__init__.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from .record_mapping import ArxivMetadataMapper
+
+from .record_routes import CATEGORY_MEMBER_ROUTE
+from .record_routes import PAPER_AUTHOR_ROUTE
+from .record_routes import PAPER_ROUTE
