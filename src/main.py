dbc09fba890f0afdcc1bdb660bd7df9118735d60
Sinclert Pérez <Sinclert@hotmail.com>
2021-10-20 14:39:10 -0400

    Define main module CLI group

--- src/main.py
+++ src/main.py
@@ -1,5 +1,40 @@
 #!/usr/bin/env python
 
+import click
+import logging
+
+from click import Context
+from logs import setup_logger
+
+logger = logging.getLogger()
+
+
+@click.group()
+@click.option(
+    "--api-url",
+    envvar="DIALECT_MAP_API_URL",
+    help="Private API base URL",
+    required=True,
+    type=str,
+)
+@click.option(
+    "--log-level",
+    envvar="DIALECT_MAP_LOG_LEVEL",
+    default="INFO",
+    help="Log messages level",
+    required=False,
+    type=str,
+)
+@click.pass_context
+def main(context: Context, api_url: str, log_level: str):
+    """Default command group for the jobs"""
+
+    setup_logger(log_level)
+
+    params = context.ensure_object(dict)
+    params["API_URL"] = api_url
+    params["LOG_LEVEL"] = log_level
+
 
 if __name__ == "__main__":
-    print("Start")
+    main()
