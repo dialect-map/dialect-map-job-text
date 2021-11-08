0047daed1f7e5b7932e2a0b9c0712a4b413d94fa
Sinclert Pérez <Sinclert@hotmail.com>
2021-11-08 17:12:16 -0500

    Simplify main module commands group

--- src/main.py
+++ src/main.py
@@ -27,13 +27,6 @@ logger = logging.getLogger()
 
 @click.group()
 @click.option(
-    "--api-url",
-    envvar="DIALECT_MAP_API_URL",
-    help="Private API base URL",
-    required=True,
-    type=str,
-)
-@click.option(
     "--log-level",
     envvar="DIALECT_MAP_LOG_LEVEL",
     default="INFO",
@@ -42,13 +35,12 @@ logger = logging.getLogger()
     type=str,
 )
 @click.pass_context
-def main(context: Context, api_url: str, log_level: str):
+def main(context: Context, log_level: str):
     """Default command group for the jobs"""
 
     setup_logger(log_level)
 
     params = context.ensure_object(dict)
-    params["API_URL"] = api_url
     params["LOG_LEVEL"] = log_level
 
 
