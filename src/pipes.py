d4b23b9a0d8f37387bea17d4168ab5f113b377c5
Sinclert <sinclert@hotmail.com>
2022-02-06 13:52:02 +0100

    Define MetadataPipeline class

--- src/pipes.py
+++ src/pipes.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from abc import ABC
 from abc import abstractmethod
+from typing import List
 
 from dialect_map_io.data_output import TextFileWriter
 
 from job.files import FileSystemIterator
+from job.input import BaseMetadataSource
 from job.input import PDFCorpusSource
+from job.mapping import CATEGORY_MEMBER_ROUTE
+from job.mapping import PAPER_AUTHOR_ROUTE
+from job.mapping import PAPER_ROUTE
+from job.models import ArxivMetadata
+from job.output import DialectMapOperator
 from job.output import LocalFileOperator
+
+logger = logging.getLogger()
 
 
 class BasePipeline(ABC):
@@ -54,3 +65,74 @@ class LocalTextPipeline(BasePipeline):
             # Save paper contents
             txt_content = self.pdf_reader.extract_txt(file_path)
             txt_operator.write_text(file_name, txt_content)
+
+
+class MetadataPipeline(BasePipeline):
+    """Pipeline extracting ArXiv metadata"""
+
+    def __init__(self, file_iter: FileSystemIterator, api_ctl: DialectMapOperator):
+        """
+        Initializes the ArXiv corpus metadata extraction pipeline
+        :param file_iter: Local file system iterator
+        :param api_ctl: API REST operator to be used as output
+        """
+
+        self.files_iterator = file_iter
+        self.api_controller = api_ctl
+        self.sources = []  # type: ignore
+
+    def _dispatch_record(self, record: ArxivMetadata) -> None:
+        """
+        Dispatch metadata record to the destination API
+        :param record: metadata record to dispatch
+        """
+
+        # The paper record must be inserted first
+        self.api_controller.create_record(PAPER_ROUTE, record.paper_record)
+
+        for membership in record.memberships_records:
+            self.api_controller.create_record(CATEGORY_MEMBER_ROUTE, membership)
+
+        for author in record.author_records:
+            self.api_controller.create_record(PAPER_AUTHOR_ROUTE, author)
+
+    def _get_metadata_records(self, paper_id: str) -> List[ArxivMetadata]:
+        """
+        Gets the metadata records from the sources given an ArXiv paper ID
+        :param paper_id: ArXiv paper metadata to get
+        :return: list of ArXiv paper metadata records
+        """
+
+        metadata_records = []
+
+        for source in self.sources:
+            metadata_records = source.get_metadata(paper_id)
+            if len(metadata_records) > 0:
+                break
+
+        return metadata_records
+
+    def add_source(self, source: BaseMetadataSource) -> None:
+        """
+        Adds an ArXiv metadata source to the list of sources
+        :param source: metadata source to extract ArXiv metadata from
+        """
+
+        self.sources.append(source)
+
+    def run(self, *args) -> None:
+        """
+        Main routine to extract ArXiv corpus metadata and send it to a REST API
+        :param args: placeholder for positional arguments (avoid MyPy errors)
+        """
+
+        for file_path in self.files_iterator.iter_paths():
+            file_name = self.files_iterator.get_file_name(file_path)
+            records = self._get_metadata_records(file_name)
+
+            if len(records) == 0:
+                logger.warning(f"Metadata for paper {file_name} not found")
+                continue
+
+            for record in records:
+                self._dispatch_record(record)
